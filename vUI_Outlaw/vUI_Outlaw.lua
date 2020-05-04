365a144f903e96712f9bc8c18d840d52925a9bc0
Hydra <ryanandrews135@gmail.com>
2020-05-04 11:59:10 -0600

    Update to meet core vUI changes

--- vUI_Outlaw/vUI_Outlaw.lua
+++ vUI_Outlaw/vUI_Outlaw.lua
@@ -1,5 +1,5 @@
 local AddOn = ...
-local vUI, GUI, Language, Media, Settings = vUIGlobal:get()
+local vUI, GUI, Language, Assets, Settings = vUIGlobal:get()
 
 local Outlaw = vUI:NewPlugin(AddOn)
 
@@ -8,7 +8,8 @@ if (vUI.UserClass ~= "ROGUE") then
 end
 
 local GetTime = GetTime
-local UnitAuraByName = UnitAuraByName
+local FindAuraByName = AuraUtil.FindAuraByName
+
 local Name, Count, Duration, Expiration, _
 
 function Outlaw:OnUpdate(elapsed)
@@ -63,39 +64,39 @@ local FrameWidth = 236
 function Outlaw:CreateBars()
 	-- Roll the Bones timer bar
 	self.BuffBar = CreateFrame("StatusBar", nil, UIParent)
-	self.BuffBar:SetStatusBarTexture(Media:GetTexture(Settings["ui-widget-texture"]))
-	self.BuffBar:SetStatusBarColorHex("E0392B")
-	self.BuffBar:SetScaledSize(FrameWidth, 10)
-	--self.BuffBar:SetScaledPoint("CENTER", UIParent, 0, -200)
-	self.BuffBar:SetScaledPoint("BOTTOM", vUI.UnitFrames["player"], "TOP", 0, 11)
+	self.BuffBar:SetStatusBarTexture(Assets:GetTexture(Settings["ui-widget-texture"]))
+	self.BuffBar:SetStatusBarColor(vUI:HexToRGB("E0392B"))
+	self.BuffBar:SetSize(FrameWidth, 10)
+	--vUI:SetPoint(self.BuffBar, "CENTER", UIParent, 0, -200)
+	self.BuffBar:SetPoint("BOTTOM", vUI.UnitFrames["player"], "TOP", 0, 11)
 	self.BuffBar:SetMinMaxValues(0, 1)
 	self.BuffBar:SetValue(0)
 	
 	self.BuffBar.BG = self.BuffBar:CreateTexture(nil, "ARTWORK")
 	self.BuffBar.BG:SetAllPoints(self.BuffBar)
-	self.BuffBar.BG:SetTexture(Media:GetTexture(Settings["ui-widget-texture"]))
-	self.BuffBar.BG:SetVertexColorHex("E0392B")
+	self.BuffBar.BG:SetTexture(Assets:GetTexture(Settings["ui-widget-texture"]))
+	self.BuffBar.BG:SetVertexColor(vUI:HexToRGB("E0392B"))
 	self.BuffBar.BG:SetAlpha(0.2)
 	
 	self.BuffBar.BG2 = self.BuffBar:CreateTexture(nil, "BORDER")
-	self.BuffBar.BG2:SetScaledPoint("TOPLEFT", self.BuffBar, -1, 1)
-	self.BuffBar.BG2:SetScaledPoint("BOTTOMRIGHT", self.BuffBar, 1, -1)
-	self.BuffBar.BG2:SetTexture(Media:GetTexture("Blank"))
+	self.BuffBar.BG2:SetPoint("TOPLEFT", self.BuffBar, -1, 1)
+	self.BuffBar.BG2:SetPoint("BOTTOMRIGHT", self.BuffBar, 1, -1)
+	self.BuffBar.BG2:SetTexture(Assets:GetTexture("Blank"))
 	self.BuffBar.BG2:SetVertexColor(0, 0, 0)
 	
 	-- Roll the bones icons
 	local IconSize = ((FrameWidth - 2) / 6) - 1
 	
 	self.RollTheBones = CreateFrame("Frame", nil, self)
-	self.RollTheBones:SetScaledPoint("BOTTOMLEFT", self.BuffBar, "TOPLEFT", -1, 0)
-	self.RollTheBones:SetScaledSize(FrameWidth + 3, IconSize + 2)
+	self.RollTheBones:SetPoint("BOTTOMLEFT", self.BuffBar, "TOPLEFT", -1, 0)
+	self.RollTheBones:SetSize(FrameWidth + 3, IconSize + 2)
 	self.RollTheBones:SetBackdrop(vUI.Backdrop)
 	self.RollTheBones:SetBackdropColor(0, 0, 0)
 	self.RollTheBones:SetBackdropBorderColor(0, 0, 0)
 	
 	for i = 1, 6 do
 		self.RollTheBones[i] = CreateFrame("Frame", nil, self.RollTheBones)
-		self.RollTheBones[i]:SetScaledSize(IconSize, IconSize)
+		self.RollTheBones[i]:SetSize(IconSize, IconSize)
 		
 		self.RollTheBones[i].Icon = self.RollTheBones:CreateTexture(nil, "ARTWORK")
 		self.RollTheBones[i].Icon:SetAllPoints(self.RollTheBones[i])
@@ -106,13 +107,13 @@ function Outlaw:CreateBars()
 		
 		self.RollTheBones[i].BG = self.RollTheBones:CreateTexture(nil, "BORDER")
 		self.RollTheBones[i].BG:SetAllPoints(self.RollTheBones[i])
-		self.RollTheBones[i].BG:SetTexture(Media:GetTexture("Blank"))
+		self.RollTheBones[i].BG:SetTexture(Assets:GetTexture("Blank"))
 		self.RollTheBones[i].BG:SetVertexColor(0, 0, 0)
 		
 		if (i == 1) then
-			self.RollTheBones[i]:SetScaledPoint("LEFT", self.RollTheBones, 1, 0)
+			self.RollTheBones[i]:SetPoint("LEFT", self.RollTheBones, 1, 0)
 		else
-			self.RollTheBones[i]:SetScaledPoint("TOPLEFT", self.RollTheBones[i-1], "TOPRIGHT", 1, 0)
+			self.RollTheBones[i]:SetPoint("TOPLEFT", self.RollTheBones[i-1], "TOPRIGHT", 1, 0)
 		end
 	end
 end
@@ -127,28 +128,3 @@ function Outlaw:Unload()
 	self.EnrageBar:Hide()
 	self:UnregisterEvent("UNIT_AURA")
 end
-
--- Test idea, remove later
-local f = CreateFrame("Frame")
-f:RegisterEvent("PLAYER_ENTERING_WORLD")
-f:SetScript("OnEvent", function(self, event)
-	local Left, Right = GUI:CreateWindow("Plugins")
-	
-	for i = 1, #vUI.Plugins do
-		if ((i % 2) == 0) then
-			Anchor = Left
-		else
-			Anchor = Right
-		end
-		
-		Anchor:CreateHeader(vUI.Plugins[i].Title)
-		
-		Anchor:CreateDoubleLine(Language["Author"], vUI.Plugins[i].Author)
-		Anchor:CreateDoubleLine(Language["Version"], vUI.Plugins[i].Version)
-		Anchor:CreateLine(" ")
-		Anchor:CreateLine(vUI.Plugins[i].Notes)
-	end
-	
-	Left:CreateFooter()
-	Right:CreateFooter()
-end)
